d27be718b72c582aaae0b8b708157006136f1fed
Kiran S <109973957+kiran1901@users.noreply.github.com>
2024-03-14 23:12:46 +0530

    init

---
+++ BombayKitchen/README.md
@@ -0,0 +1,5 @@
+## Local Installation
+- `` npm install ``
+- `` npm run dev ``
+
+# https---github.com-Bombay-Kitchen-BombayKitchenUI-tree-init
---
+++ BombayKitchen/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import Header from './layouts/Header.vue';
+import Main from './layouts/Main.vue';
+import Footer from './layouts/Footer.vue';
+
+</script>
+
+<template>
+    <Header/>
+
+    <Main/>
+
+    <Footer />
+</template>
+
+<style scoped>
+Main {
+    font-family:'Source Sans Pro', sans-serif;
+	font-weight: 500;
+	margin: 0px;
+	padding: 0px;
+	padding-bottom: 100px;
+	font-size: 14px;
+}
+</style>
